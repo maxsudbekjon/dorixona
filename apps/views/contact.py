556e97589001f4d5e3a60dc45a17bbe3412f5007
Ozodbek <ozodbekprimov69@gmail.com>
2026-02-11 16:44:29 +0500

    .

--- apps/views/contact.py
+++ apps/views/contact.py
@@ -1,6 +1,7 @@
-from rest_framework import generics
+from rest_framework import generics, permissions
 from apps.models import ContactRequest
 from apps.Serializers import ContactRequestSerializer
 class ContactRequestCreateAPIView(generics.CreateAPIView):
     queryset=ContactRequest.objects.all()
     serializer_class = ContactRequestSerializer
+    permission_classes = [permissions.AllowAny]
