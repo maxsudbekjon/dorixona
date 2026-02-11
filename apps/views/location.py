3b4f804c39aaa58892f8b5558eb81657b53cc2c8
Ozodbek <ozodbekprimov69@gmail.com>
2026-02-11 18:05:41 +0500

    add search

--- apps/views/location.py
+++ apps/views/location.py
@@ -1,7 +1,11 @@
 from rest_framework import generics
 from apps.models.Location import Location
 from apps.Serializers import LocationModelSerializer
+from rest_framework import filters
 
 class LocationListAPIView(generics.ListAPIView):
     queryset=Location.objects.all()
     serializer_class = LocationModelSerializer
+    filter_backends = [filters.SearchFilter]
+
+    search_fields = ['title', 'address']
