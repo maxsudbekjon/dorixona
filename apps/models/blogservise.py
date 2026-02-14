a7f537509e15d3c5ddc99f0047cb473eb902bf3e
ChoriyevJamshid <jamshidchoriyev709@gmail.com>
2026-02-14 15:48:19 +0500

    ,.

--- apps/models/blogservise.py
+++ apps/models/blogservise.py
@@ -10,7 +10,11 @@ class ServiceCategory(models.Model):
 
 
 class Service(models.Model):
-    category = models.ForeignKey(ServiceCategory, on_delete=models.CASCADE, related_name='services')
+    category = models.ForeignKey(ServiceCategory,
+                                 on_delete=models.CASCADE,
+                                 related_name='services',
+                                 blank=True,
+                                 null=True)
     title = models.CharField(max_length=255)
     image = models.ImageField(upload_to='services/')
     description = models.TextField(blank=True, null=True)
@@ -19,32 +23,31 @@ class Service(models.Model):
         return self.title
 
 
-
 class Resource(models.Model):
-    name = models.CharField(max_length=255) # Figma: "U.S. Food and Drug Administration"
-    url = models.URLField() # Figma: "www.fda.gov"
+    name = models.CharField(max_length=255)  # Figma: "U.S. Food and Drug Administration"
+    url = models.URLField()  # Figma: "www.fda.gov"
 
     def __str__(self):
         return self.name
 
 
 class BlogPost(models.Model):
-    title = models.CharField(max_length=255) # Figma: "Understanding Your Medications..."
-    image = models.ImageField(upload_to='blog/') # Figma: Rasm
-    created_at = models.DateField() # Figma: "Jan 11, 2026"
-    description = models.TextField() # Postning ichki matni
+    title = models.CharField(max_length=255)  # Figma: "Understanding Your Medications..."
+    image = models.ImageField(upload_to='blog/')  # Figma: Rasm
+    created_at = models.DateField()  # Figma: "Jan 11, 2026"
+    description = models.TextField()  # Postning ichki matni
 
     def __str__(self):
         return self.title
 
 
 class Review(models.Model):
-    full_name = models.CharField(max_length=100) # Figma: "Emily Carter"
-    avatar = models.ImageField(upload_to='reviews/', blank=True, null=True) # Figma: User rasmi
+    full_name = models.CharField(max_length=100)  # Figma: "Emily Carter"
+    avatar = models.ImageField(upload_to='reviews/', blank=True, null=True)  # Figma: User rasmi
     rating = models.PositiveIntegerField(
         validators=[MinValueValidator(1), MaxValueValidator(5)]
-    ) # Figma: Yulduzchalar (1 dan 5 gacha)
-    message = models.TextField() # Figma: "Friendly staff and fast service..."
+    )  # Figma: Yulduzchalar (1 dan 5 gacha)
+    message = models.TextField()  # Figma: "Friendly staff and fast service..."
 
     def __str__(self):
         return f"{self.full_name} ({self.rating} stars)"
